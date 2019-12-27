cf92076c8a1298684b3667230e1af240182adacc
Orb_H <orb.cov@gmail.com>
2019-12-27 21:58:36 +0900

    now accepts user input of daily query

--- APICaller/user_input.py
+++ APICaller/user_input.py
@@ -1,10 +1,19 @@
 from . import kart_query as kq
+import datetime
 
 qc = kq.QueryCaller(5)
+qc.start()
 
 while True:
     s = input()
     t = s.split()
     if t[0].lower() == 'stop':
         break
+    if t[0].lower() == 'daily':
+        try:
+            y, m, d = t[1].split('-')
+            dqo = kq.DailyQueryObject(datetime.date(y, m, d), '스피드 개인전')
+            qc.add_query(dqo)
+        except:
+            pass
 qc.stop()
